46911fe116013cc3c304027660cc884773d61f97
Francisco José Serrano Muñoz <franciscojoseserranomunoz@gmail.com>
2026-06-11 14:34:29 +0200

    BreadCrumbs, CustomMenu y arreglo de componentes

---
+++ src/components/custom/CustomBreadcrumbs.tsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router";
+import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "../ui/breadcrumb"
+import { SlashIcon } from "lucide-react";
+
+interface Breadcrumb {
+    label: string;
+    to: string;
+}
+
+interface Props {
+    currentPage: string;
+    breadcrumbs?: Breadcrumb[]; 
+}
+
+export const CustomBreadcrumbs = ({ currentPage, breadcrumbs = [] }: Props) => {
+    return (
+        <Breadcrumb className="my-5">
+            <BreadcrumbList>
+                <BreadcrumbItem>
+                    <BreadcrumbLink asChild>
+                        <Link to="/">Home</Link>
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                
+                <BreadcrumbSeparator>
+                    <SlashIcon/>
+                </BreadcrumbSeparator>
+                
+                {
+                    breadcrumbs.map(crumb => (
+                        <div className="flex items-center">
+                            <BreadcrumbItem>
+                                <BreadcrumbLink asChild>
+                                    <Link to={crumb.to}>{crumb.label}</Link>
+                                </BreadcrumbLink>
+                                <BreadcrumbSeparator>
+                                    <SlashIcon/>
+                                </BreadcrumbSeparator>
+                            </BreadcrumbItem>
+                        </div>
+                    ))
+                }
+
+                <BreadcrumbItem>
+                    <BreadcrumbLink className="text-black">{currentPage}</BreadcrumbLink>
+                </BreadcrumbItem>
+            </BreadcrumbList>
+        </Breadcrumb>
+    )
+}
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,5 +1,7 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
 
 export const SearchPage = () => {
     return (
@@ -8,9 +10,19 @@ export const SearchPage = () => {
                 title="SuperHeroes Search"
                 description="Discover, explore and admin superheroes and villains"
             />
+            <CustomBreadcrumbs currentPage="Search Heroes"
+                // breadcrumbs={[
+                //     { label: 'Home1', to: '/'},
+                //     { label: 'Home2', to: '/'},
+                //     { label: 'Home3', to: '/'},
+                // ]}
+            />
 
             {/* Stats Dashboard */}
             <HeroStats />
+
+            {/* Filter and search */}
+            <SearchControls />
         </>
     )
 }
