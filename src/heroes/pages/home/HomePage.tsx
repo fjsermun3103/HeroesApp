3db8b2ec43e0526d8cbb6094611a0940025c40b7
Francisco José Serrano Muñoz <franciscojoseserranomunoz@gmail.com>
2026-06-11 23:35:06 +0200

    Componente de paginación

--- src/heroes/pages/home/HomePage.tsx
+++ src/heroes/pages/home/HomePage.tsx
@@ -17,6 +17,8 @@ export const HomePage = () => {
     const [ searchParams, setSearchParams ] = useSearchParams();
 
     const activeTab = searchParams.get('tab') ?? 'all';
+    const page = searchParams.get('page') ?? '1';
+    const limit = searchParams.get('limit') ?? '6';
 
     const selectedTab = useMemo(() => {
         const validTabs = ['all', 'favorites', 'heroes', 'villains'];
@@ -25,8 +27,8 @@ export const HomePage = () => {
 
 
     const { data: heroesResponse } = useQuery({
-        queryKey: ['heroes'],
-        queryFn: () => getHeroesByPageAction(),
+        queryKey: ['heroes', { page, limit }],
+        queryFn: () => getHeroesByPageAction(+page, +limit),
         staleTime: 1000 * 60 * 5, // 5 minutos
     })
 
@@ -107,7 +109,7 @@ export const HomePage = () => {
                 </Tabs>
 
                 {/* Pagination */}
-                <CustomPagination totalPages={8} />
+                <CustomPagination totalPages={heroesResponse?.pages ?? 1} />
             </>
         </>
     );
