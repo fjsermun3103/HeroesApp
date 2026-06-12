7127aac6f32f9c191bea6273cc2bf417201b05b5
Francisco José Serrano Muñoz <franciscojoseserranomunoz@gmail.com>
2026-06-12 13:46:18 +0200

    Aplicar filtros de búsqueda, acordeon y filtrar por fuerza

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -2,8 +2,24 @@ import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "@/heroes/components/HeroStats";
 import { SearchControls } from "./ui/SearchControls";
 import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { HeroGrid } from "@/heroes/components/HeroGrid";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
+import { searchHeroesAction } from "@/heroes/actions/search-heroes.action";
 
 export const SearchPage = () => {
+
+    const [ searchParams ] = useSearchParams();
+
+    const name = searchParams.get('name') ?? undefined;
+    const strength = searchParams.get('strength') ?? undefined;
+
+    const { data: heroes = [] } = useQuery({
+        queryKey: ['search', { name, strength }],
+        queryFn: () => searchHeroesAction({name, strength}),
+        staleTime: 1000 * 60 * 5, // 5 min
+    })
+
     return (
         <>
             <CustomJumbotron
@@ -23,6 +39,9 @@ export const SearchPage = () => {
 
             {/* Filter and search */}
             <SearchControls />
+
+            {/* */}
+            <HeroGrid heroes={heroes}></HeroGrid>
         </>
     )
 }
